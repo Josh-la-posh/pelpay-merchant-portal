a101a95acfcc9be15869cadbed2598ded787d080
Josh-la-posh <joshuamayawa23@yahoo.com>
2025-08-29 15:27:53 +0100

    Updated

--- src/redux/slices/complianceSlice.js
+++ src/redux/slices/complianceSlice.js
@@ -19,8 +19,6 @@ const complianceSlice = createSlice({
             state.complianceError = null;
         },
         complianceSuccess: (state, action) => {
-            console.log("Compliance Success Data: ", action.payload)
-            // console.log("I am working noww")
             state.complianceLoading = false;
             state.complianceData = action.payload?.data ?? action.payload;
             state.complianceSuccess = true;
